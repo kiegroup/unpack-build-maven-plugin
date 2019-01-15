6722fa6b5110837aad43c276682a2c4a5f219a84
tzimanyi <tibor.zimanyi@gmail.com>
2019-01-15 16:06:10 +0100

    Add asserts to test

--- unpack-build-maven-plugin-itests/src/test/java/org/kie/unpackbuildplugin/integrationtests/UnpackBuildIT.java
+++ unpack-build-maven-plugin-itests/src/test/java/org/kie/unpackbuildplugin/integrationtests/UnpackBuildIT.java
@@ -2,6 +2,7 @@ package org.kie.unpackbuildplugin.integrationtests;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,6 +10,8 @@ import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
 import org.apache.maven.it.util.ResourceExtractor;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UnpackBuildIT {
 
@@ -20,12 +23,35 @@ public class UnpackBuildIT {
         final Verifier verifier = new Verifier(testDir.getAbsolutePath());
         try {
             verifier.executeGoal("clean");
-
             verifier.executeGoal("verify", getProperties());
             verifier.verifyErrorFreeLog();
         } finally {
             verifier.resetStreams();
         }
+
+        final File checkoutRootDirectory = new File(testDir, "checkout-temp");
+        final File mavenPluginProjectDirectory = new File(checkoutRootDirectory, "unpack-build-maven-plugin");
+        final File mavenPluginTestsDirectory = new File(checkoutRootDirectory, "unpack-build-maven-plugin-itests");
+
+        assertThat(checkoutRootDirectory).exists().isDirectory();
+        assertThat(mavenPluginProjectDirectory).exists().isDirectory();
+        assertThat(mavenPluginTestsDirectory).exists().isDirectory();
+
+        assertThat(new File(mavenPluginTestsDirectory, "target")).doesNotExist();
+
+        assertMavenPluginProjectDirectory(mavenPluginProjectDirectory);
+    }
+
+    private void assertMavenPluginProjectDirectory(final File mavenPluginProjectDirectory) {
+        assertThat(new File(mavenPluginProjectDirectory, "target")).exists().isDirectory();
+        assertThat(Paths.get(mavenPluginProjectDirectory.getAbsolutePath(), "target", "classes").toFile())
+                .exists().isDirectory();
+        assertThat(Paths.get(mavenPluginProjectDirectory.getAbsolutePath(), "target", "classes", "META-INF").toFile())
+                .exists().isDirectory();
+        assertThat(Paths.get(mavenPluginProjectDirectory.getAbsolutePath(), "target", "classes", "META-INF", "MANIFEST.MF").toFile())
+                .exists().isFile();
+        assertThat(Paths.get(mavenPluginProjectDirectory.getAbsolutePath(), "target", "classes", "org", "kie", "unpackbuildplugin", "UnpackBuildMojo.class").toFile())
+                .exists().isFile();
     }
 
     private Map<String, String> getProperties() {
