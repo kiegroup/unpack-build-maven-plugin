97414693caaa653423822d97ce2b1d45c1209b06
tzimanyi <tibor.zimanyi@gmail.com>
2019-01-23 14:29:22 +0100

    Add support for maven-archetype packaging

--- unpack-build-maven-plugin/src/main/java/org/kie/unpackbuildplugin/util/ArtifactPackaging.java
+++ unpack-build-maven-plugin/src/main/java/org/kie/unpackbuildplugin/util/ArtifactPackaging.java
@@ -3,7 +3,7 @@ package org.kie.unpackbuildplugin.util;
 import java.util.Arrays;
 
 public enum ArtifactPackaging {
-    POM, JAR, WAR, BUNDLE, MAVEN_PLUGIN;
+    POM, JAR, WAR, BUNDLE, MAVEN_PLUGIN, MAVEN_ARCHETYPE;
 
     private static final String FILETYPE_POM = "pom";
     private static final String FILETYPE_JAR = "jar";
@@ -22,6 +22,7 @@ public enum ArtifactPackaging {
             case WAR: return FILETYPE_WAR;
             case BUNDLE: return FILETYPE_JAR;
             case MAVEN_PLUGIN: return FILETYPE_JAR;
+            case MAVEN_ARCHETYPE: return FILETYPE_JAR;
             default:
                 throw new IllegalArgumentException(String.format(Messages.UNKNOWN_ARTIFACT_PACKAGING, artifactPackaging));
         }
@@ -30,6 +31,8 @@ public enum ArtifactPackaging {
     private static ArtifactPackaging getArtifactPackagingFromString(final String stringValue) {
         if (MAVEN_PLUGIN.toString().equals(stringValue.toUpperCase())) {
             return MAVEN_PLUGIN;
+        } else if (MAVEN_ARCHETYPE.toString().equals(stringValue.toUpperCase())) {
+            return MAVEN_ARCHETYPE;
         } else {
             return ArtifactPackaging.valueOf(stringValue.toUpperCase());
         }
@@ -39,6 +42,8 @@ public enum ArtifactPackaging {
     public String toString() {
         if (this == MAVEN_PLUGIN) {
             return "MAVEN-PLUGIN";
+        } else if (this == MAVEN_ARCHETYPE) {
+            return "MAVEN-ARCHETYPE";
         } else {
             return super.toString();
         }
