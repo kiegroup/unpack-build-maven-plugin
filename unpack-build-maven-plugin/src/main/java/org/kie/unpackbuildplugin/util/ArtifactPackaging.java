b94e66501f380ebab40136256acc8e90d3d9f0e1
tzimanyi <tibor.zimanyi@gmail.com>
2019-01-28 15:47:38 +0100

    Add support for gwt-lib packaging

--- unpack-build-maven-plugin/src/main/java/org/kie/unpackbuildplugin/util/ArtifactPackaging.java
+++ unpack-build-maven-plugin/src/main/java/org/kie/unpackbuildplugin/util/ArtifactPackaging.java
@@ -3,7 +3,14 @@ package org.kie.unpackbuildplugin.util;
 import java.util.Arrays;
 
 public enum ArtifactPackaging {
-    POM, JAR, WAR, BUNDLE, MAVEN_PLUGIN, MAVEN_ARCHETYPE, KJAR;
+    POM,
+    JAR,
+    WAR,
+    BUNDLE,
+    MAVEN_PLUGIN,
+    MAVEN_ARCHETYPE,
+    KJAR,
+    GWT_LIB;
 
     private static final String FILETYPE_POM = "pom";
     private static final String FILETYPE_JAR = "jar";
@@ -17,13 +24,22 @@ public enum ArtifactPackaging {
 
     public static String convertToFileType(final String artifactPackaging) {
         switch (getArtifactPackagingFromString(artifactPackaging)) {
-            case POM: return FILETYPE_POM;
-            case JAR: return FILETYPE_JAR;
-            case WAR: return FILETYPE_WAR;
-            case BUNDLE: return FILETYPE_JAR;
-            case MAVEN_PLUGIN: return FILETYPE_JAR;
-            case MAVEN_ARCHETYPE: return FILETYPE_JAR;
-            case KJAR: return FILETYPE_JAR;
+            case POM:
+                return FILETYPE_POM;
+            case JAR:
+                return FILETYPE_JAR;
+            case WAR:
+                return FILETYPE_WAR;
+            case BUNDLE:
+                return FILETYPE_JAR;
+            case MAVEN_PLUGIN:
+                return FILETYPE_JAR;
+            case MAVEN_ARCHETYPE:
+                return FILETYPE_JAR;
+            case KJAR:
+                return FILETYPE_JAR;
+            case GWT_LIB:
+                return FILETYPE_JAR;
             default:
                 throw new IllegalArgumentException(String.format(Messages.UNKNOWN_ARTIFACT_PACKAGING, artifactPackaging));
         }
@@ -34,6 +50,8 @@ public enum ArtifactPackaging {
             return MAVEN_PLUGIN;
         } else if (MAVEN_ARCHETYPE.toString().equals(stringValue.toUpperCase())) {
             return MAVEN_ARCHETYPE;
+        } else if (GWT_LIB.toString().equals(stringValue.toUpperCase())) {
+            return GWT_LIB;
         } else {
             return ArtifactPackaging.valueOf(stringValue.toUpperCase());
         }
@@ -45,6 +63,8 @@ public enum ArtifactPackaging {
             return "MAVEN-PLUGIN";
         } else if (this == MAVEN_ARCHETYPE) {
             return "MAVEN-ARCHETYPE";
+        } else if (this == GWT_LIB) {
+            return "GWT-LIB";
         } else {
             return super.toString();
         }
